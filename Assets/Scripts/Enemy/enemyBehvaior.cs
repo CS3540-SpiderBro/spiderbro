c8f9015a0d6ae9010218b8ecb80cda1934158613
Greg Hatfield <hatfieldgreg@hotmail.com>
2015-09-30 15:33:29 -0600

    Added files to branch
    Added spawner and enemyBehvaior (misspelled .cs file lol) not
    implemented into the game yet

---
+++ Assets/Scripts/Enemy/enemyBehvaior.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// move enemies towards motherbase and delete them when collide with base
+/// </summary>
+public class enemyBehvaior : MonoBehaviour {
+
+    public float speed = 5.0f; // move speed
+    public Transform motherbase;
+    public GameObject mbj;
+    public int damage;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        mbj = GameObject.FindGameObjectWithTag("MotherBase");
+        // move towards motherbase
+        if (mbj != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, motherbase.position, speed);
+        }
+        
+    }
+
+
+    void OnCollisionEnter(Collision touched)
+    {
+       
+        if (touched.gameObject.tag == "MotherBase")
+        {
+            
+            Destroy(this.gameObject);            
+           
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner/spawner.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// spawn ccontrol center, mass spawn at first then waves after, randomize spawn locations based on x and z coordinates
+/// </summary>
+public class spawner : MonoBehaviour {
+    public Rigidbody aEnemy;
+    public int xMin;
+    public int xMax;
+    public int zMin;
+    public int zMax;
+    public int numEnemies;
+    public int activeEnemies;
+	public bool SpawnON = true;
+    int enemyCount;
+    
+    // Use this for initialization
+    void Start () {       
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+        enemyCount = GameObject.FindGameObjectsWithTag("EnemyUnit").Length;
+        Debug.Log(enemyCount);
+        if (activeEnemies <= 0 || enemyCount <= 1)
+        {
+            
+            // Do your enemy spawns here
+            for (int i = 0; i < numEnemies; i++)
+            {                
+                SpawnEnemy();
+            }
+            activeEnemies += numEnemies;
+            // Reset for next spawn
+           
+        }
+    }
+
+
+
+    public void SpawnEnemy()
+    {
+
+        Vector3 newPos = new Vector3(Random.Range(xMin, xMax),250, Random.Range(zMin, zMax));
+		Rigidbody aEnemyClone = (Rigidbody) Instantiate(aEnemy, newPos, Quaternion.identity);
+        
+       
+    }
+
+	public void switchSpawn(bool state)
+	{
+		SpawnON = state;
+	}
+}

7cde3c8a07f0d0d4c81a266644061eee70b85c87
Darth Futuza <darthfutuza@gmail.com>
2015-10-13 13:18:36 -0600

    DFutuza's changes, attack1 now works along with replacing player controller and adding spider model

--- Assets/Scripts/NPC/EnemyStats.cs
+++ Assets/Scripts/NPC/EnemyStats.cs
@@ -6,7 +6,7 @@ public class EnemyStats : MonoBehaviour
 
 	const int MAX_HP = 500;
 
-	public int hp;
+	public static int hp = 100;
 	public int xp;
 	public bool is_alive;
 
@@ -17,20 +17,30 @@ public class EnemyStats : MonoBehaviour
 		xp = 25;	//xp to be worth
 		is_alive = true;
 	}
+
+	public int checkHP()
+	{
+		return hp;
+	}
+
+	public void changeHP(int change)
+	{
+		hp = change;
+	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
 
 
-		if (hp > 1) 
+		/*if (hp < 1) 
 		{
 			is_alive = false;	//cannot damage/kill/interact with objects that are not alive
 			//play death sound
 
 			//give player xp
-			Destroy (gameObject, 5);	//destroy enemy after 5 seconds
-		}
+			Destroy (gameObject, 0);	//destroy enemy after 5 seconds
+		}*/
 
 
 

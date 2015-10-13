6b2cad10b7d5f4a8de3a6d679b86baf874af1bb8
Darth Futuza <darthfutuza@gmail.com>
2015-10-13 09:44:16 -0600

    changing scene to prep for merge

---
+++ Assets/Scripts/NPC/EnemyStats.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyStats : MonoBehaviour 
+{
+
+	const int MAX_HP = 500;
+
+	public int hp;
+	public int xp;
+	public bool is_alive;
+
+	// Use this for initialization
+	void Start () 
+	{
+		hp = 100;	//set life to 100
+		xp = 25;	//xp to be worth
+		is_alive = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+
+		if (hp > 1) 
+		{
+			is_alive = false;	//cannot damage/kill/interact with objects that are not alive
+			//play death sound
+
+			//give player xp
+			Destroy (gameObject, 5);	//destroy enemy after 5 seconds
+		}
+
+
+
+	}
+}

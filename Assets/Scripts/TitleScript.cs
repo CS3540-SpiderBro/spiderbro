da4d00922c39a93d022f62c9cdc7d84f996d1072
Darth Futuza <darthfutuza@gmail.com>
2015-10-13 17:16:04 -0600

    Fixed start game script (to use space instead of mouse1 (wasn't working))

--- Assets/Scripts/TitleScript.cs
+++ Assets/Scripts/TitleScript.cs
@@ -10,7 +10,7 @@ public class TitleScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    if(Input.GetMouseButtonDown(0))
+		if(Input.GetKeyDown("space") ) 
         {
             Application.LoadLevel("Level0");
         }

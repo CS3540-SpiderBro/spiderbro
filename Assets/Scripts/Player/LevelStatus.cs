e9c1cda3d28eb63cb60c5f537a32532481563fd0
Greg Hatfield <hatfieldgreg@hotmail.com>
2015-10-14 17:54:27 -0600

    fixed the scripts mouth to levelstatus
    proper game handling code for milestone1

--- Assets/Scripts/Player/LevelStatus.cs
+++ Assets/Scripts/Player/LevelStatus.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
-
+//added code from mouthScript into here - Greg
 public class LevelStatus : MonoBehaviour
 {
     public float TOTAL_SECONDS = 10.0f;
@@ -11,8 +11,11 @@ public class LevelStatus : MonoBehaviour
 	public Text killCountText;
 	public Text timerText;
 	public Text crossHair;
-	
-	private int _killCount;
+
+    GameObject[] gameObjects;
+    GameObject[] spawnObjects;
+    mouthScript mouth;
+    private int _killCount;
 	private bool gameOver;
     private float secondsLeft;
 
@@ -34,7 +37,12 @@ public class LevelStatus : MonoBehaviour
 			return;
 
 		DisplayKillCount ();
-
+        mouth = GameObject.FindObjectOfType(typeof(mouthScript)) as mouthScript;
+        if(mouth.getStatus() == true)
+        {
+            winText.text = "You Lost!!";//changed time is up to You have Won, and call DeleteAll method
+            DeleteAll();
+        }
         // decrease timer to 0
         secondsLeft = Mathf.Max(secondsLeft - Time.deltaTime, 0);
         // set timer text
@@ -44,13 +52,15 @@ public class LevelStatus : MonoBehaviour
         if (gameOver = secondsLeft == 0)
 		{
 			Destroy(crossHair);
-			winText.text = "Time is Up!!";
+			winText.text = "You have Won!!";//changed time is up to You have Won, and call DeleteAll method
+            DeleteAll();
 		} 
-		else if ( gameOver = EnemyCount == _killCount)
-		{
-			Destroy (crossHair);
-			winText.text = "You Win!";
-		}
+		//else if ( gameOver = EnemyCount == _killCount)
+		//{
+		//	Destroy (crossHair);
+		//	winText.text = "You Win!";
+		//}
+
 	}
 
 	public void AddKill(int score = 1)
@@ -64,6 +74,24 @@ public class LevelStatus : MonoBehaviour
 		killCountText.text = string.Format("Kill Count: {0}", _killCount.ToString());
 		
 	}
+
+
+    void DeleteAll()
+    {
+        gameObjects = GameObject.FindGameObjectsWithTag("Enemy");
+        for (int i = 0; i < gameObjects.Length; i++)
+        {
+            Destroy(gameObjects[i]);
+        }
+        spawnObjects = GameObject.FindGameObjectsWithTag("spawner");
+        for (int i = 0; i < spawnObjects.Length; i++)
+        {
+            Destroy(spawnObjects[i]);
+        }
+              
+        timerText.text = "Time: 0:00.00";
+
+    }
 }
 
 

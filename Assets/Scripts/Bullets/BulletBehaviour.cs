6bd48195a013fb519f0eb417fdefdc6c1e58ebfa
Greg Hatfield <hatfieldgreg@hotmail.com>
2015-10-13 18:29:01 -0600

    dont worry part duex
    doing something that fixed spawner but had some file issues.

---
+++ Assets/Scripts/Bullets/BulletBehaviour.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+
+public class BulletBehaviour : MonoBehaviour {
+
+	public float lifespan = 3.0f;
+	public GameObject explode;
+	public LevelStatus levelStatus;
+
+    public GameObject bloodsplat;
+    public GameObject Bulletsplat;
+
+    // Use this for initialization
+    void Start () {
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null) 
+		{
+			levelStatus = gameControllerObject.GetComponent<LevelStatus>();	
+		}
+
+		if (levelStatus == null)
+		{
+			Debug.Log ("Cannot find levelStatus from game controller");
+		}
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if ((lifespan -= Time.deltaTime) <= 0) {
+			Disappear ();
+		}
+
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		Instantiate(Bulletsplat, gameObject.transform.position, gameObject.transform.rotation);
+        if (col.gameObject.CompareTag ("Enemy")) {
+			try{
+
+				KillEnemy(col);
+				levelStatus.AddKill();
+
+
+			}
+			catch (Exception e){
+				Debug.Log(e.Message);
+			}
+
+		}
+        Destroy(gameObject);
+    }
+
+    void Disappear ()
+	{
+		Destroy (gameObject);
+	}
+
+	void KillEnemy(Collision col){
+		col.gameObject.tag = "Dead";
+		Instantiate(bloodsplat, gameObject.transform.position, gameObject.transform.rotation);
+        Destroy(col.gameObject);
+	}
+}
---
+++ Assets/Scripts/Player/LevelStatus.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LevelStatus : MonoBehaviour
+{
+    public const float TOTAL_SECONDS = 10.0f;
+
+    public int EnemyCount;
+	public Text winText;
+	public Text killCountText;
+	public Text timerText;
+	public Text crossHair;
+	
+	private int _killCount;
+	private bool gameOver;
+    private float secondsLeft;
+
+
+    // Use this for initialization
+    void Start () 
+	{
+		winText.text = "";
+		_killCount = 0;
+		gameOver = false;
+        secondsLeft = TOTAL_SECONDS;
+		DisplayKillCount ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		if (gameOver)
+			return;
+
+		DisplayKillCount ();
+
+        // decrease timer to 0
+        secondsLeft = Mathf.Max(secondsLeft - Time.deltaTime, 0);
+        // set timer text
+        timerText.text = string.Format("Time: {0}:{1:00.00}", (int)(secondsLeft / 60), secondsLeft % 60);
+
+
+        if (gameOver = secondsLeft == 0)
+		{
+			Destroy(crossHair);
+			winText.text = "Time is Up!!";
+		} 
+		else if ( gameOver = EnemyCount == _killCount)
+		{
+			Destroy (crossHair);
+			winText.text = "You Win!";
+		}
+	}
+
+	public void AddKill(int score = 1)
+	{
+		++_killCount;
+		DisplayKillCount();
+	}
+
+	void DisplayKillCount()
+	{
+		killCountText.text = string.Format("Kill Count: {0}", _killCount.ToString());
+		
+	}
+}
+
+

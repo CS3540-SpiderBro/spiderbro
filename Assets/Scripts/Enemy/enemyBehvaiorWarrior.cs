29c4c9c470a944e2e435aff1c531b8286d63444e
Darth Futuza <darthfutuza@gmail.com>
2015-12-12 23:53:45 -0700

    Fixing up script on flying chair (flying warriors)

--- Assets/Scripts/Enemy/enemyBehvaiorWarrior.cs
+++ Assets/Scripts/Enemy/enemyBehvaiorWarrior.cs
@@ -11,16 +11,24 @@ public class enemyBehvaiorWarrior : MonoBehaviour {
     public float speed = 0.2f; // move speed
 	public float mindist = 8.0f;
     Vector3 playerTarget;
-    //GameObject mbj;
     public string targetTag;
     public string damagerTag;
-   // public int damage;
     public float RotationSpeed;
     private Quaternion _lookRotation;
     private Vector3 _direction;
 
+	//bullets
+	public Rigidbody bullet;			//the bullet
+	private float bulletforce = 100.0f;
+	public float SPIT_CAPACITY = 2f;
+	public float SPIT_REGEN_RATE = 0.25f;
+	private float spitAmmo;
+	
+
+
 	//for sounds
 	public AudioClip deathSound;
+	public AudioClip fireProjectile;	//sound projectile makes
 	private AudioSource source;
 
     // Use this for initialization
@@ -51,6 +59,7 @@ public class enemyBehvaiorWarrior : MonoBehaviour {
 		if(playerTarget != null)
         {
             Move();
+			attackPlayer();
         }
         
 			//Debug.Log("playerTarget position" + playerTarget);
@@ -71,12 +80,30 @@ public class enemyBehvaiorWarrior : MonoBehaviour {
 
 
 		float distance = Vector3.Distance (transform.position, playerTarget);	//find distance between us and player
-		Debug.Log ("Distance is: " + distance);
+		//Debug.Log ("Distance is: " + distance);
 		if (distance > mindist) 
 		{
 			transform.position = Vector3.MoveTowards (transform.position, playerTarget, speed);
 		}	//if we're further away from player than mindist, get closer
     }
+
+	void attackPlayer()
+	{
+		spitAmmo = Mathf.Min(SPIT_CAPACITY, spitAmmo + SPIT_REGEN_RATE * Time.deltaTime);
+
+		if (spitAmmo >= 1f) 
+		{
+			source.PlayOneShot (fireProjectile, 1.0F);
+			
+			//Rigidbody projectile = (Rigidbody)Instantiate (bullet, playerTarget.transform.position + playerTarget.transform.forward, playerTarget.transform.rotation);
+			Rigidbody projectile = (Rigidbody)Instantiate (bullet, transform.position, Quaternion.identity);
+			projectile.AddForce (Vector3.forward * bulletforce, ForceMode.Impulse);
+			spitAmmo -= 1f;
+		}
+
+		Debug.Log ("spitAmmo: " + spitAmmo);
+	}
+
     void OnCollisionEnter(Collision touched)
     {
        //if we get hit by projectile, time to explode
